8527e2241566bc17a51bf757e6756a5510c320f2
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2022-08-04 09:29:47 +0300

    fixes based on tutroial

--- src/main/java/com/epam/task/exceptions/PriceDiscountPurchase.java
+++ src/main/java/com/epam/task/exceptions/PriceDiscountPurchase.java
@@ -1,12 +1,17 @@
 package com.epam.task.exceptions;
 
+import com.epam.task.exceptions.customExc.NonPositiveArgumentException;
+import com.epam.task.exceptions.customExc.OutOfBoundArgumentException;
+
 public class PriceDiscountPurchase extends AbstractPurchase{
     private final Euro discountAmount;
 
     public PriceDiscountPurchase(Product product, int purchasedNum, Euro discountAmount) {
         super(product, purchasedNum);
         if(product.getPrice().compareTo(discountAmount) <= 0)
-            throw new IllegalArgumentException("Discount can't be bigger than price");
+            throw new OutOfBoundArgumentException("Discount can't be bigger than price");
+        if (discountAmount.equals(new Euro(0)))
+            throw new NonPositiveArgumentException("Discount can't be zero");
         this.discountAmount = discountAmount;
     }
 
--- src/main/java/com/epam/task/exceptions/PurchaseFactory.java
+++ src/main/java/com/epam/task/exceptions/PurchaseFactory.java
@@ -1,9 +1,15 @@
 package com.epam.task.exceptions;
 
+import com.epam.task.exceptions.customExc.CsvLineException;
+import com.epam.task.exceptions.customExc.EmptyArgumentException;
+import com.epam.task.exceptions.customExc.NonPositiveArgumentException;
+import com.epam.task.exceptions.customExc.OutOfBoundArgumentException;
+
 public class PurchaseFactory {
-    public AbstractPurchase createPurchase(String[] values) {
-        if (values.length == 3) {
-            try {
+    private enum PurchaseKind {
+        PURCHASE {
+            @Override
+            protected AbstractPurchase getPurchase(String[] values) {
                 return new Purchase(
                         new Product(
                                 values[0],
@@ -11,12 +17,11 @@ public class PurchaseFactory {
                         ),
                         Integer.parseInt(values[2])
                 );
-            } catch (Exception ignored) {
             }
-        }
-
-        if (values.length == 4) {
-            try {
+        },
+        PRICE_DISCOUNT_PURCHASE {
+            @Override
+            protected AbstractPurchase getPurchase(String[] values) {
                 return new PriceDiscountPurchase(
                         new Product(
                                 values[0],
@@ -25,10 +30,31 @@ public class PurchaseFactory {
                         Integer.parseInt(values[2]),
                         new Euro(Integer.parseInt(values[3]))
                 );
-            } catch (Exception ignored) {
             }
-        }
+        };
 
-        return null;
+        protected abstract AbstractPurchase getPurchase(String[] values) throws NumberFormatException, NonPositiveArgumentException,
+                EmptyArgumentException;
+    }
+
+    private PurchaseKind getPurchaseKind(String[] values) throws OutOfBoundArgumentException {
+        if(values.length == 3)
+            return PurchaseKind.PURCHASE;
+        if (values.length == 4)
+            return  PurchaseKind.PRICE_DISCOUNT_PURCHASE;
+        throw new OutOfBoundArgumentException("Illegal amount of values");
+    }
+
+    public AbstractPurchase createPurchase(String line) throws CsvLineException{
+        return createPurchase(line.split(";"));
+    }
+
+    public AbstractPurchase createPurchase(String[] values) throws CsvLineException {
+        try{
+            return getPurchaseKind(values).getPurchase(values);
+        } catch (OutOfBoundArgumentException | NumberFormatException | NonPositiveArgumentException |
+                EmptyArgumentException e) {
+            throw new CsvLineException(values, e);
+        }
     }
 }
